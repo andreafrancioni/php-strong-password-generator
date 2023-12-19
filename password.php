a3772dea622fec8b4dafbfc550b6e8b955cd59c5
andreafrancioni <andreafrancioni.work@gmail.com>
2023-12-19 18:07:58 +0100

    crea funzione per generare password recuperando valori da array con metodo array_rand

--- password.php
+++ password.php
@@ -1,6 +1,20 @@
 <?php
 $passwordLength = $_GET["passwordLength"];
 
-$arrayCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-$arrayNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-$arraySpecialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '?'];
+$arrayCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '?'];
+// $arrayNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// $arraySpecialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '?'];
+
+function generaPassword($passwordLength, $arrayCharacters)
+{
+    $generatedPassword = "";
+    for ($i = 0; $i < $passwordLength; $i++) {
+        $casualCharacters = $arrayCharacters[array_rand($arrayCharacters)];
+
+        $generatedPassword .= $casualCharacters;
+    }
+
+    return $generatedPassword;
+}
+
+echo generaPassword($passwordLength, $arrayCharacters);
